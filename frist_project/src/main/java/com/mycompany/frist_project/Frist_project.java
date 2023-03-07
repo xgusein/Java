3415685ba3d48f43efb367bbde9535b4e22a7cd0
xgusein <ibadzadehuseyn@gmail.com>
2023-03-08 02:47:15 +0400

    dsjhdgisjfk

---
+++ frist_project/src/main/java/com/mycompany/frist_project/Frist_project.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.frist_project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author huseynibadzade
+ */
+public class Frist_project {
+
+    public static void main(String[] args) {
+//        System.out.print("Salam");
+//        System.out.println("welcome");
+          Scanner scanner = new Scanner(System.in);
+          
+          System.out.println("Enter age: ");
+          
+//          int age = scanner.nextInt();
+//          
+//          System.out.println("Age: " + age);
+          
+//          double number = scanner.nextDouble();
+//          
+//          System.out.println("Number: " + number);
+            String yazi = scanner.nextLine();
+            System.out.println("yazi:" + yazi);
+          
+    }
+}
