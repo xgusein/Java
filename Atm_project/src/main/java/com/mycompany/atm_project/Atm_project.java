51b22f379e388d2bfe1c33fe440e2ee889da5a54
xgusein <ibadzadehuseyn@gmail.com>
2023-03-29 01:52:34 +0400

    Atm

--- Atm_project/src/main/java/com/mycompany/atm_project/Atm_project.java
+++ Atm_project/src/main/java/com/mycompany/atm_project/Atm_project.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.atm_project;
 
+import java.util.Scanner;
+
 /**
  *
  * @author huseynibadzade
@@ -11,6 +13,50 @@ package com.mycompany.atm_project;
 public class Atm_project {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int balance = 1000;
+        String works = "1. Work : Account Info\n"
+                        + "2. Work : Withdrawl Money\n"
+                        + "3. Work : Deposite Money\n"
+                        + "Press q for Exit";
+        System.out.println("*******************************");
+        System.out.println(works);
+        System.out.println("*******************************");
+        
+        while (true) {            
+            System.out.println("Isleminizi Seciniz: ");
+            String work = scanner.nextLine();
+            
+            if (work.equals("q")) {
+                System.out.println("Exiting Program");
+                break;
+            }
+            else if (work.equals("1")) {
+                System.out.println("Balance "+ balance);
+                
+            }
+            else if (work.equals("2")) {
+                System.out.println("Amount of money: ");
+                int amount = scanner.nextInt();
+                scanner.nextLine();
+                
+                if (balance - amount <0) {
+                    System.out.println("there is no enaugh money. Balance:" + balance);
+                }
+                else {
+                    balance -= amount;
+                    System.out.println("New balance: " + balance);
+                }
+            }
+            else if (work.equals("3")) {
+                int amount = scanner.nextInt();
+                scanner.nextLine();
+                balance += amount;
+                System.out.println("New Balance: " + balance);
+            }
+            else {
+                System.out.println("Wrong work");
+            }
+        }
     }
 }
